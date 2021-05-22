ad93460ce9d8a74d1e128ae1d9a5e68dcc0e8118
Mansi Khamkar <khamkarmansi@gmail.com>
2021-05-23 01:58:27 +0530

    Create LC_Rotate_Image.cpp

---
+++ LC_Rotate_Image.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/rotate-image/
+
+class Solution {
+// Logic : Transpose and reflect (flip) the matrix
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int temp;
+        int n = matrix.size();
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n/2;j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+    }
+};
