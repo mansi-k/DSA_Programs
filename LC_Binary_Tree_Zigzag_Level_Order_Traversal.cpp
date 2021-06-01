d814624bcfd8bec5920a190f0e0a14eab834f78f
Mansi Khamkar <khamkarmansi@gmail.com>
2021-06-02 00:37:06 +0530

    Update LC_Binary_Tree_Zigzag_Level_Order_Traversal.cpp

--- LC_Binary_Tree_Zigzag_Level_Order_Traversal.cpp
+++ LC_Binary_Tree_Zigzag_Level_Order_Traversal.cpp
@@ -48,3 +48,48 @@ public:
         return zigzag;
     }
 };
+
+
+// ****** Using Deque *******
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root)
+            return ans;
+        deque<TreeNode*> dq;
+        dq.push_back(root);
+        bool isLTR = true;
+        while(!dq.empty()) {
+            int n = dq.size();
+            vector<int> currLevel;
+            if(isLTR) { // if left to right
+                for(int i=0;i<n;i++) { // parse from queue front, and push childnodes left to right
+                    auto curr = dq.front();
+                    dq.pop_front();
+                    currLevel.push_back(curr->val);
+                    if(curr->left)
+                        dq.push_back(curr->left);
+                    if(curr->right)
+                        dq.push_back(curr->right);
+                }
+                isLTR = false; // change direction flag
+            } 
+            else { // if right to left
+                for(int i=0;i<n;i++) { // parse from queue tail, and push childnodes right to left
+                    auto curr = dq.back(); 
+                    dq.pop_back();
+                    currLevel.push_back(curr->val);
+                    if(curr->right)
+                        dq.push_front(curr->right);
+                    if(curr->left)
+                        dq.push_front(curr->left);
+                }
+                isLTR = true; // change direction flag
+            }
+            ans.push_back(currLevel);
+        }
+        return ans;
+    }
+};
